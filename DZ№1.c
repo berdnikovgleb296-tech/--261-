d8511470a32fa730a678cd025943d07246c06fa2
berdnikovgleb296-tech <berdnikovgleb296@gmail.com>
2026-09-14 00:43:15 +0300

    Add files via upload

---
+++ DZ№1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	puts ("        ______________");
+    puts ("       /              \");
+    puts ("      /                \");
+    puts ("     /                  \");
+    puts ("    |  __            __  |");
+    puts ("    | |  |          |  | |");
+    puts ("    | |__|          |__| |");
+    puts ("  __|____________________|__");
+    puts (" /                          \");
+    puts ("/____________________________\");
+    puts ("|  []                    []  |");
+    puts ("     (O)              (O)");
+    puts ("    (   )            (   )");
+    puts ("     (O)              (O)");
+    
+
+    return 0;
+}
